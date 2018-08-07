a290617588f030a10c1cf7a6471bc7aab47a6ea2
Ezekiel Giordano <ezekielgiordano@gmail.com>
2018-08-07 14:15:01 -0400

    Added sports

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,56 @@ User.create!(
   password: '111111',
   role: 'admin'
 )
+
+Sport.create!(
+  name: 'boxing',
+  description: '(from Wikipedia) Boxing is a combat sport in which two people, usually wearing protective gloves, throw punches at each other for a predetermined set of time in a boxing ring.'
+)
+Sport.create!(
+  name: 'wrestling',
+  description: '(from Wikipedia) Wrestling is a combat sport involving grappling type techniques such as clinch fighting, throws and takedowns, joint locks, pins and other grappling holds.'
+)
+Sport.create!(
+  name: 'BJJ',
+  description: '(from Wikipedia) Brazilian jiu-jitsu is a martial art and combat sport system that focuses on grappling and especially ground fighting.'
+)
+Sport.create!(
+  name: 'judo',
+  description: '(from Wikipedia) Judo[...]is generally categorized as a modern martial art[...]whose prominent feature is its competitive element where the objective is to either throw or takendown an opponent to the ground, immobilize or otherwise subdue an opponent[...].'
+)
+Sport.create!(
+  name: 'jiu-jitsu',
+  description: '(from Wikipedia) Jiu-jitsu is a Japanese martial art and a method of close combat for defeating an armed and armored opponent in which one uses either a short weapon or none.'
+)
+Sport.create!(
+  name: 'karate',
+  description: '(from Wikipedia) Karate is[...]predominantly a striking art using punching, kicking, knee strikes, elbow strikes and open-hand techniques such as knife-hands, spear-hands, and palm-heel strikes.'
+)
+Sport.create!(
+  name: 'kung fu',
+  description: '(from Wikipedia) Chinese martial arts, often named under the umbrella terms kung fu, are the several hundred fighting styles that have developed over the centuries in China.'
+)
+Sport.create!(
+  name: 'aikido',
+  description: '(from Wikipedia) Aikido is a modern Japanese martial art developed by Morihei Ueshiba as a synthesis of his martial studies, philosophy, and religious beliefs.'
+)
+Sport.create!(
+  name: 'sumo',
+  description: '(from Wikipedia) Sumo wrestling is a competitive full-contact wrestling sport where a wrestler attempts to force another wrestler out of a circular ring or into touching the ground with anything other than the soles of his feet.'
+)
+Sport.create!(
+  name: 'Muay Thai',
+  description: '(from Wikipedia) Muay Thai is a combat sport of Thailand that uses stand-up striking along with various clinching techniques.'
+)
+Sport.create!(
+  name: 'taekwondo',
+  description: '(from Wikipedia) Taekwondo is a Korean martial art characterized by its emphasis on head-height kicks, jumping and spinning kicks, and fast kicking techniques.'
+)
+Sport.create!(
+  name: 'kickboxing',
+  description: '(from Wikipedia) Kickboxing is a group of stand-up combat sports based on kicking and punching, historically developed from karate mixed with boxing.'
+)
+Sport.create!(
+  name: 'savate',
+  description: '(from Wikipedia) Savate is a French combat sport that uses the hands and feet as weapons combining elements of English boxing with graceful kicking techniques.'
+)
