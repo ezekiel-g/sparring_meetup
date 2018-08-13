52c3ce82241ebba1a452f457ea42af3c78d3c587
Ezekiel Giordano <ezekielgiordano@gmail.com>
2018-08-13 17:26:03 -0400

    Added delete functionality, tweaked serializers

--- app/serializers/proposal_serializer.rb
+++ app/serializers/proposal_serializer.rb
@@ -1,7 +1,39 @@
 class ProposalSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :sport_id, :description, :username
+  attributes :id, :user_id, :sport_id, :description, :username, :sport, :email, :age, :gender, :height, :weight, :reach
 
   def username
     object.user.username
+  end
+
+  def sport
+    object.sport.name.upcase
+  end
+
+  def email
+    object.user.email
+  end
+
+  def age
+    object.user.age
+  end
+
+  def gender
+    object.user.gender
+  end
+
+  def height
+    object.user.height
+  end
+
+  def weight
+    object.user.weight
+  end
+
+  def reach
+    if object.user.reach != nil
+      return object.user.reach
+    else
+      return 'N/A'
+    end
   end
 end
